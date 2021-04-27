0227b9df79f7b6d328a506811c2d619f3040c729
webdev <garggaurav628@gmail.com>
2021-04-27 22:52:59 +0530

    changes

--- activity/homepage.js
+++ activity/homepage.js
@@ -5,3 +5,22 @@ const request=require("request");
 const fs=require("fs");
 
 const cheerio=require("cheerio");
+
+//async
+// request("https://www.espncricinfo.com/series/ipl-2020-21-1210595",getdata);
+
+
+// function getdata(error,response,body)
+// {
+//     fs.writeFileSync("./homepage.html",body);
+// }
+
+let homepage=fs.readFileSync("./homepage.html");
+
+let ch=cheerio.load(homepage);
+
+let atag=ch(".widget-items.cta-link a");
+
+let allmatchlink= "https://www.espncricinfo.com"+ atag['0'].attribs.href;
+
+console.log(allmatchlink);
