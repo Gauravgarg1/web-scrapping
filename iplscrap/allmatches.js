d6a6a10c0204eed2e03a4863ace94404797173a9
webdev <garggaurav628@gmail.com>
2021-04-28 21:40:58 +0530

    all details batsmen inserted in local machine

--- iplscrap/allmatches.js
+++ iplscrap/allmatches.js
@@ -1,5 +1,6 @@
 const request = require("request");
 const cheerio = require("cheerio");
+const getMatchDetails = require("./match");
 
 function getAllMatches(link){
     request(link , function(error , response , data){
@@ -12,12 +13,13 @@ function processData(html){
     // get liks of all the matchess !!!
     let ch = cheerio.load(html);
     let allATags = ch('a[data-hover="Scorecard"]');
+    // { {} , {} , {} , {} , {} , {} , {} , {} }
     for(let i=0 ; i<allATags.length ; i++){
         let matchLink = "https://www.espncricinfo.com"+ch(allATags[i]).attr("href");
-        console.log(matchLink);
+        getMatchDetails(matchLink); //60times
     }
 }
 
 
-
+// node js feature
 module.exports = getAllMatches;
